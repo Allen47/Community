68c49a754a3e5e632f322ee0aec048a27351bde1
Allen47 <1371908769@qq.com>
2020-11-24 00:22:36 +0800

    delete surplus code

--- community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,12 +1,8 @@
 package com.nowcoder.community;
 
-import com.nowcoder.community.dao.AlphaDao;
-import com.nowcoder.community.service.AlphaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -31,23 +27,8 @@ public class CommunityApplicationTests implements ApplicationContextAware {
 	@Test
 	public void testApplicationContext() {//test 从容器中获取bean
 		System.out.println(applicationContext);
-
-		AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
-		System.out.println(alphaDao.select());
-
-		alphaDao = applicationContext.getBean("alphaHibernate", AlphaDao.class);
-		System.out.println(alphaDao.select());
 	}
 
-	@Test
-	public void testBeanManagement() {
-		AlphaService alphaService = applicationContext.getBean(AlphaService.class);
-		System.out.println(alphaService);
-
-		//验证被Spring容器管理的对象是否默认仅仅实例化一次:是
-		alphaService = applicationContext.getBean(AlphaService.class);
-		System.out.println(alphaService);
-	}
 
 	@Test
 	public void testBeanConfig() {
@@ -56,21 +37,5 @@ public class CommunityApplicationTests implements ApplicationContextAware {
 		System.out.println(simpleDateFormat.format(new Date()));
 	}
 
-	@Autowired
-	@Qualifier("alphaHibernate")
-	private AlphaDao alphaDao;
-
-	@Autowired
-	private AlphaService alphaService;
-
-	@Autowired
-	private SimpleDateFormat simpleDateFormat;
-
-	@Test
-	public void testDI() {
-		System.out.println(alphaDao);
-		System.out.println(alphaService);
-		System.out.println(simpleDateFormat);
-	}
-
 }
+
