f407329eaeb1b3478eef15a745b81f8e939f5172
Allen47 <1371908769@qq.com>
2020-11-30 17:31:10 +0800

    修复LoginController bug

--- community/src/main/java/com/nowcoder/community/controller/LoginController.java
+++ community/src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.nowcoder.community.controller;
 import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,18 +23,14 @@ import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.Map;
-
-import static com.nowcoder.community.util.CommunityConstant.DEFAULT_EXPIRED_SECONDS;
-import static com.nowcoder.community.util.CommunityConstant.REMEMBER_EXPIRED_SECONDS;
 
 /**
  * @author Msq
  * @date 2020/11/24 - 1:18
  */
 @Controller
-public class LoginController {
+public class LoginController implements CommunityConstant{
 
     @Autowired
     private UserService userService;
@@ -54,16 +52,33 @@ public class LoginController {
     @RequestMapping(path = "/register", method = RequestMethod.POST)
     public String register(Model model, User user){
         Map<String, Object> map = userService.register(user);
-        if(map== null || map.isEmpty()){ //没有任何错误信息
-            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
-            model.addAttribute("target","/index");
+        if(map == null || map.isEmpty()) { //没有任何错误信息
+            model.addAttribute("msg", "注册成功,我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
+            model.addAttribute("target", "/index");
             return "/site/operate-result";
-        }else {
+        } else {
             model.addAttribute("usernameMsg", map.get("usernameMsg"));
             model.addAttribute("passwordMsg", map.get("passwordMsg"));
-            model.addAttribute("emailMsg", map.get("emilMsg"));
-            return "site/register";
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            return "/site/register";
         }
+    }
+
+    // http://localhost:8080/community/activation/101/code
+    @RequestMapping(path = "/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "激活成功,您的账号已经可以正常使用了!");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作,该账号已经激活过了!");
+            model.addAttribute("target", "/index");
+        } else {
+            model.addAttribute("msg", "激活失败,您提供的激活码不正确!");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
     }
 
     @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
@@ -103,7 +118,7 @@ public class LoginController {
             cookie.setPath(contextPath);
             cookie.setMaxAge(expiredSeconds);
             response.addCookie(cookie);
-            return "/redirect:/index";
+            return "redirect:/index";
 
         }else{ //无凭证
             model.addAttribute("usernameMsg", map.get("usernameMsg"));
@@ -113,8 +128,9 @@ public class LoginController {
     }
 
     @RequestMapping(path = "/logout", method = RequestMethod.GET)
-    public String logout(@CookieValue("ticket") String ticket){
+    public String logout(@CookieValue("ticket") String ticket) {
         userService.logout(ticket);
         return "redirect:/login";
     }
+
 }
