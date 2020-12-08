0e560262d232d0d2cdc726aee2299580707132f5
Allen47 <1371908769@qq.com>
2020-12-09 00:09:22 +0800

    修正数据库错误bug

---
+++ community/src/main/java/com/nowcoder/community/annotation/LoginRequired.java
@@ -0,0 +1,8 @@
+package com.nowcoder.community.annotation;
+
+/**
+ * @author Msq
+ * @date 2020/12/7 - 13:06
+ */
+public @interface LoginRequired {
+}
---
+++ community/src/main/java/com/nowcoder/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,8 @@
+package com.nowcoder.community.controller.interceptor;
+
+/**
+ * @author Msq
+ * @date 2020/12/7 - 13:09
+ */
+public class LoginRequiredInterceptor {
+}
